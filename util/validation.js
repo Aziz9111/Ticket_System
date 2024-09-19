950a6f783a9c8c9c6763b84689e74a386cbc38ba
Aziz <abdulazizbinafif11@gmail.com>
2024-09-19 20:17:56 +0300

    created validations function

--- util/validation.js
+++ util/validation.js
@@ -1,3 +1,30 @@
 function isEmpty(value) {
   return !value || value.trim() === "";
 }
+
+function userValidCredentials(email, password) {
+  return (
+    email && email.includes("@") && password && password.trim().length >= 6
+  );
+}
+
+function validUserData(name, email, password) {
+  return(
+  userValidCredentials(email, password) &&
+  !isEmpty(name)
+  );
+}
+
+function passwordConfirmed(password, confirmedPassword) {
+  return password === confirmedPassword;
+}
+
+function emailConfirmed(email, confirmedEmail) {
+  return email === confirmedEmail;
+}
+
+module.exports = {
+  validUserData: validUserData,
+  passwordConfirmed: passwordConfirmed, 
+  emailConfirmed: emailConfirmed
+}
