96eb8a317c9df2c47075bad3509a618efaa8ff37
Aziz <abdulazizbinafif11@gmail.com>
2024-10-05 19:47:28 +0300

    limit file size

--- middleware/image-upload.js
+++ middleware/image-upload.js
@@ -1,13 +1,16 @@
 const multer = require("multer");
 const uuid = require("uuid");
 
+const maxSize = 1000 * 1000 * 2;
+
 const upload = multer({
     storage: multer.diskStorage({
         destination:"ticket-data/images",
         filename: (req, file, cb) => {
             cb(null, uuid() + "-" + file.originalname);
         }
-    })
+    }), 
+    limits: { fileSize: maxSize /* bytes */ }
 });
 
 const configuredMulterMiddleware = upload.single("image");
