d95e5ca4ba649845e1fb96027d52dad6deaec063
Aziz <abdulazizbinafif11@gmail.com>
2024-10-14 12:40:29 +0300

    adding some functions related to updating a ticket

--- models/ticket.model.js
+++ models/ticket.model.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 const db = require("../data/database");
 const path = require("path");
 
@@ -9,15 +10,33 @@ class Ticket {
     this.imagePath = ticketData.image ? path.join(__dirname, '../../Images', ticketData.image) : null; 
     this.imageUrl = this.image ? `/images/${ticketData.image}` : null;
     this.user_email = ticketData.user_email;
+    this.status = ticketData.status;
+    this.type = ticketData.type;
+    this.priority = ticketData.priority;
+    this.project = ticketData.project;
+    this.reply = ticketData.reply;
+    this.id = ticketData.id;
   }
 
   static async findAll() {
-    const [data] = await db.query("SELECT title, description, user_email FROM ticket");
+    const [data] = await db.query("SELECT * FROM ticket");
     return data.map((ticketData) => new Ticket(ticketData)); // Map through data
   }
 
-  static async findById(id) {
-    await db.query("SELECT * FROM ticket WHERE id = ?", [id]);
+  static async findOne(id, email) {
+    const [data] = await db.query("SELECT * FROM ticket WHERE id = ? and user_email = ?", [id, email]);
+    if (data.length === 0) {
+      return null; // Return null if no ticket is found
+  }
+    return new Ticket(data[0]);
+  }
+
+  static async findOneId(id) {
+    const [data] = await db.query("SELECT * FROM ticket WHERE id = ?", [id]);
+    if (data.length === 0) {
+      return null; // Return null if no ticket is found
+  }
+    return new Ticket(data[0]);
   }
   
   async save() {
@@ -41,6 +60,38 @@ class Ticket {
       console.error("Error saving ticket:", error);
       throw error;
     }
+  }
+
+  static async adminUpdateTicket(ticketId, priority, reply) {
+    const [data] = await db.query(
+      "UPDATE ticket SET priority = ?, reply = ? WHERE id = ?", 
+      [priority, reply, ticketId]);
+
+    return [data];
+  }
+
+  static async type(typeName) {
+    const [data] = await db.query(
+      "INSERT INTO type (name) VALUE (?)",
+      [typeName]
+    );
+    return [data];
+  }
+
+  static async project(projectName, projectDescription) {
+    const [data] = await  db.query(
+      "INSERT INTO project (name, description) VALUES (?, ?)",
+      [projectName, projectDescription]
+    );
+    return [data];
+  }
+
+  static async agentUpdateTicket(ticketId, status, reply) {
+    const [data] = await db.query(
+      "UPDATE ticket SET status = ?, reply = ? WHERE id = ?", 
+      [status, reply, ticketId]);
+
+    return [data];
   }
   
   
