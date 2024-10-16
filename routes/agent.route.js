182591c319230b9b39838e8e7266b8ecad1afe15
Aziz <abdulazizbinafif11@gmail.com>
2024-10-16 21:57:19 +0300

    Added agent routs

--- routes/agent.route.js
+++ routes/agent.route.js
@@ -7,5 +7,11 @@ router.get("/agent/tickets", agentController.getAllTickets);
 
 router.get("/agent/ticket/:id", agentController.viewTicket);
 
+router.post("/agent/ticket/:id", agentController.updateTicket);
+
+router.get("/agent/send-ticket/:id", agentController.getSendTicket);
+
+router.post("/agent/send-ticket/:id", agentController.postSendTicket);
+
 
 module.exports = router;
