1315de83df29d8158b723393c90c6a683913b923
Aziz <abdulazizbinafif11@gmail.com>
2024-11-10 07:30:58 +0300

    Modification in the function

--- config/mail.js
+++ config/mail.js
@@ -4,27 +4,27 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const transport = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    secure: false,
-    auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS
-    },
-})
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
 
-const sendEmail = async ({userEmail, ticketId}) => {
-    /* console.log("Sending email to:", userEmail); // To see if it working correctlly
+const sendEmail = async ({ userEmail, ticketId }) => {
+  /* console.log("Sending email to:", userEmail); // To see if it working correctlly
     console.log("Ticket ID:", ticketId); */
-    if (!userEmail) {
-        throw new Error("User email is not defined.");
-    }
-    return await transport.sendMail({
-        from: "aziz.storage911@gmail.com",
-        to: userEmail,
-        subject: `Your Ticket Number is: #${ticketId}`,
-        text: `Thank you for your submission! Your ticket number is: #${ticketId}.`,
-    })
-}
+  if (!userEmail) {
+    throw new Error("User email is not defined.");
+  }
+  return await transport.sendMail({
+    from: "aziz.storage911@gmail.com",
+    to: userEmail,
+    subject: `Your Ticket Number is: #${ticketId}`,
+    text: `Thank you for your submission! Your ticket number is: #${ticketId}.`,
+  });
+};
 
 module.exports = sendEmail;
